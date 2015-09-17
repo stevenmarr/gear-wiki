5a29523e93c914a24e3d0fa917ba7f3df9d9f4e6
stevenmarr <stevenmarr@me.com>
2015-09-17 09:30:51 -0600

    pull credential data out

--- setting.py
+++ setting.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python
 import json
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
-APPLICATION_NAME = 'Gear Wiki'
 
+#APPLICATION ********************
+APPLICATION_NAME = 'Gear Wiki'
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+SECRET_KEY = json.loads(open('secrets.json', 'r').read())['app']['secret_key']
+
+
+#DATA BASE ************************
+#path for database
+#for local production, comment for server
+DATA_BASE = 'sqlite:///gear_wiki.db'
+#for server, comment for local production
+#DB_PASS = json.loads(open('secrets.json', 'r').read())['app']['database_pass']
+#DATA_BASE = 'postgresql://postgres:%s@localhost/gearopedia', % DB_PASS
+
+
+#FILES ***************************
+#folder paths
 UPLOAD_FOLDER = './files/uploads'
 UPLOAD_IMG_FOLDER = './files/img'
+#permitted image and file extensions
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'dmg'}
 ALLOWED_IMG_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-DATA_BASE = 'sqlite:///gear_wiki.db'
-
+#file types available for selection from model forms, required server restart
 FILE_TYPE = {
 	'0': '-',
     '1': 'Owners Manual',
@@ -20,4 +34,6 @@ FILE_TYPE = {
     '5': 'Other',
     }
 
-SECRET_KEY = '9EE3195AAD26ED3B76DB642552A3A'
+
+
+
